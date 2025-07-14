3a2a361557253e435a6560ce3186cd467a3ef978
ardanozp <ardanozpolat256@gmail.com>
2025-07-14 15:03:25 +0300

    Initial Commit

---
+++ Day02-TemperatureConverter/ConverterTests/ConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterTests.swift
+//  ConverterTests
+//
+//  Created by ardano on 14.07.2025.
+//
+
+import Testing
+@testable import Converter
+
+struct ConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
