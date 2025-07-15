6042e7adca0a34c25359f080123d4c0dcd6ef0bb
ardanozp <ardanozpolat256@gmail.com>
2025-07-15 19:15:51 +0300

    Initial Commit

---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by ardano on 14.07.2025.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
