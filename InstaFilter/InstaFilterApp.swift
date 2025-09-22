43d52a95093b781f7ff0788e39b2dbf376fa91fd
ardanozp <ardanozpolat256@gmail.com>
2025-09-22 16:32:54 +0300

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by ardano on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
